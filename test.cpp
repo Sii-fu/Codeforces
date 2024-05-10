298b9d01cded031ac9d18288ba69165350554578
Sifat Bin Asad <sifatbinasad@gmail.com>
2024-05-10 16:56:56 +0600

    solved vs code problem

--- test.cpp
+++ test.cpp
@@ -1,15 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <cstring>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long
-#define pb push_back
-#define mp make_pair
-#define pii pair<int, int>
 
 const int MOD = 1e9 + 7;
 const int INF = 1e9;
@@ -17,6 +7,9 @@ const int INF = 1e9;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    int n;
+    cin >> n;
+    cout << n << endl;
 
     // Your code here
 
